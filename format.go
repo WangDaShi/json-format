42b6f3c8be7e053ef7636c6601c6e1d9015676a6
Loatr99 <1530805767@qq.com>
2022-12-11 23:17:33 +0800

    '添加测试文件'

--- format.go
+++ format.go
@@ -1,6 +1,34 @@
 package main
 
-func main(){
-    println("test")
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	path := "D://code/go/json-format/test.json"
+	println(Format(path))
 }
 
+func Format(filePath string) string {
+	content := readFileToString(filePath)
+	return content
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// read file and return the content
+func readFileToString(filePath string) string {
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		fmt.Printf("file %s not exist !\n", filePath)
+		panic(err)
+	}
+	data, err := os.ReadFile(filePath)
+	check(err)
+	return string(data)
+}
